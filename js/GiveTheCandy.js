bf96ef7c18beb63d77122e27ddb785b01a390a55
Lacey Walker <bluelarksong@gmail.com>
2020-11-05 22:15:26 -0600

    minor changes to setup

---
+++ js/GiveTheCandy.js
@@ -0,0 +1,19 @@
+alert("WARNING: Always say no to their demands. There is crying in baseball.")
+
+const please = ["child1"]
+
+const child1 = {
+    plea: "Please can I fill the tub with Mountain Dew?"
+
+}
+
+console.log(child1.plea);
+
+
+
+var name = prompt("What is your name?");
+alert(`Hi, ${name}. Want to see something cool?`);
+document.querySelector('html').innerHTML = `
+  <h1>Welcome to ${name}'s site!</h1>
+  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif">
+`;
--- scripts/GiveTheCandy.js
+++
@@ -1,8 +0,0 @@
-const please = ["child1"]
-
-const child1 = {
-    plea: "Please can I fill the tub with Mountain Dew?"
-
-}
-
-console.log(child1.plea);
